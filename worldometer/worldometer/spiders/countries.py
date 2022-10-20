342a5686ca4e2bc547a939fd8ac9f631ce440e04
Derek Shing <hpshing@gmail.com>
2022-10-19 19:00:41 -0700

    worldometer finish

--- worldometer/worldometer/spiders/countries.py
+++ worldometer/worldometer/spiders/countries.py
@@ -11,9 +11,21 @@ class CountriesSpider(scrapy.Spider):
         for country in countries:
             name = country.xpath(".//text()").get()
             link = country.xpath(".//@href").get()
-            yield response.follow(url=link, callback=self.parse_country)
+            #pass variable to another parse function by meta arg
+            yield response.follow(url=link, callback=self.parse_country, meta ={'country_name':name})
 
 
 
     def parse_country(self,response):
+        #get the variable from the page before by request.meta
+        name = response.request.meta['country_name']
         logging.info(response.url)
+        rows = response.xpath("(//table[@class='table table-striped table-bordered table-hover table-condensed table-list'])[1]/tbody/tr")
+        for row in rows:
+            year = row.xpath("./td[1]/text()").get()
+            population = row.xpath(".//strong/text()").get()
+            yield {
+                'name':name,
+                'year': year,
+                'population': population
+            }
